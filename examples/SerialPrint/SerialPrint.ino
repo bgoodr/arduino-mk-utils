c4ff7fa0a96a6f14774709a5836eae8f65bfb74d
Brent Goodrick <bgoodr@gmail.com>
2015-05-24 13:20:15 -0700

    Summary: Fix formatting by using spaces and an Emacs mode setting
    Otherwise, the formatting looks weird when rendered by GitHub.

--- examples/SerialPrint/SerialPrint.ino
+++ examples/SerialPrint/SerialPrint.ino
@@ -1,10 +1,12 @@
+// -*-mode: c++; indent-tabs-mode: nil; -*-
+
 void setup() {
-	Serial.begin(9600);
-	Serial.print("Press any key: ");
+  Serial.begin(9600);
+  Serial.print("Press any key: ");
 }
 
 void loop() {
-	if (Serial.available()) {
-		Serial.println(Serial.read());
-	}
+  if (Serial.available()) {
+    Serial.println(Serial.read());
+  }
 }
